d655f6962eb12f1a4f8775f920bd9f10a089498a
Leyva <oleyva@cps.k12.il.us>
2024-08-30 15:06:30 -0500

    first commit

---
+++ lesson2.py
@@ -0,0 +1,54 @@
+# #create a print funtion that prints out the string "Hello, World!"
+# print("Hello World!")
+# # ask the user for the day of the week 
+# # date_of_week = input("What day of the week is it?")
+# # print("Today is"+ date_of_week)
+# #concatenation is when you add two strings together 
+# # using a plus sign (+)
+# movies_this_week = input("What movies are you watching this week?")
+# print("I am watching"+ movies_this_week + "this week")
+# mood = input("How are you feeling today?")
+# print("I am feeling"+ mood )
+
+# data types for variables in python
+
+# strings - text
+# name = "John" # this is a string data type
+# # whenever it is wrapped in quotes it is a string
+# year = "2024"
+
+# # integers - whole numbers
+# year = 2024 # this is an integer data type
+# # does not need to be wrapped in quotes
+# yearFourFromNow = 2028
+# subtract = yearFourFromNow - year
+# print(subtract)
+
+# # floats = decimal numbers
+# priceBigMac = 3.99 # this is a float data type
+# priceDoublePounder = 4.99
+# totalPrice = priceBigMac + priceDoublePounder
+# print (totalPrice)
+
+# # booleans - True or False
+# isRaining = False # this is a boolean data type
+# print(isRaining)
+
+# # lists - a collection of items
+# groceries = ["apples", "bananas", "carrots"]
+# print(groceries)
+
+name_of_movie = "Spiderman"
+
+time_of_movie = "6:00 PM"
+
+time_of_dinner = "7:45 PM"
+
+number_of_people = 4
+
+name_of_restaurant = "Portillos"
+
+address_of_restaurant = "3425 W Kilbourn St"
+
+print("I am going to watch", name_of_movie , "at", time_of_movie , "today and then go at" , time_of_dinner , "to", name_of_restaurant, "located at", address_of_restaurant, "with", number_of_people, "people" )
+
